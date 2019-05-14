d4d9f928edb272ce9f705ad32ec664250d4208a3
Antoine Lessard <antoinix93@hotmail.com>
2019-05-14 18:17:13 -0400

    Cartes additionnelles

--- app/src/main/java/ca/csf/mobile1/yogioh/DeckAdapter.java
+++ app/src/main/java/ca/csf/mobile1/yogioh/DeckAdapter.java
@@ -1,8 +1,6 @@
 package ca.csf.mobile1.yogioh;
 
 import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
